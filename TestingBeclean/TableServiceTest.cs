9045ee282416e46b375833faac5aec2bb8e62651
Șuchea Rareș-Andrei <rasuchea75@gmail.com>
2024-05-02 21:42:31 +0300

    added databaseService tests

--- TestingBeclean/TableServiceTest.cs
+++ TestingBeclean/TableServiceTest.cs
@@ -1,34 +1,34 @@
-﻿using NUnit.Framework;
+﻿using System.Data.SqlClient;
+using NUnit.Framework;
 using Moq;
+using NUnit.Framework.Legacy;
+using SuperbetBeclean;
 using SuperbetBeclean.Model;
 using SuperbetBeclean.Services;
 using SuperbetBeclean.Windows;
-using SuperbetBeclean;
-using System.Data.SqlClient;
-using NUnit.Framework.Legacy;
 
 namespace TestingBeclean
 {
     [TestFixture]
     public class TableServiceTest
     {
-        private TableService _tableService;
-        private Mock<CardDeck> _mockCardDeck;
-        private Mock<DataBaseService> _mockDatabaseService;
-        private Mock<SqlConnection> _mockSqlConnection;
-        private Mock<HandRankCalculator> _mockRankCalculator;
-        private Mock<MainService> _mockMainService;
+        private TableService tableService;
+        private Mock<CardDeck> mockCardDeck;
+        private Mock<DataBaseService> mockDatabaseService;
+        private Mock<SqlConnection> mockSqlConnection;
+        private Mock<HandRankCalculator> mockRankCalculator;
+        private Mock<MainService> mockMainService;
 
         [SetUp]
         public void Setup()
         {
-            _mockRankCalculator = new Mock<HandRankCalculator>();
-            _mockCardDeck = new Mock<CardDeck>();
-            _mockDatabaseService = new Mock<DataBaseService>();
-            _mockSqlConnection = new Mock<SqlConnection>();
-            _tableService = new TableService(100, 5, 10, "TestTable", _mockDatabaseService.Object); // Pass null for database service
-            //_tableService.RankCalculator = _mockRankCalculator.Object;
-            _mockMainService = new Mock<MainService>();
+            mockRankCalculator = new Mock<HandRankCalculator>();
+            mockCardDeck = new Mock<CardDeck>();
+            mockDatabaseService = new Mock<DataBaseService>();
+            mockSqlConnection = new Mock<SqlConnection>();
+            tableService = new TableService(100, 5, 10, "TestTable", mockDatabaseService.Object); // Pass null for database service
+            // _tableService.RankCalculator = _mockRankCalculator.Object;
+            mockMainService = new Mock<MainService>();
         }
 
         [Test]
@@ -36,17 +36,17 @@ namespace TestingBeclean
         {
             // Arrange
             var expectedCard = new PlayingCard(PlayingCard.CARD_VALUES["A"], PlayingCard.HEART_SYMBOL); // Example card
-            _mockCardDeck.Setup(deck => deck.GetDeckSize()).Returns(1); // Mock deck size
-            _mockCardDeck.Setup(deck => deck.GetCardFromIndex(It.IsAny<int>())).Returns(expectedCard); // Mock getting card from index
-            _mockCardDeck.Setup(deck => deck.RemoveCardFromIndex(It.IsAny<int>())); // Mock removing card from index
+            mockCardDeck.Setup(deck => deck.GetDeckSize()).Returns(1); // Mock deck size
+            mockCardDeck.Setup(deck => deck.GetCardFromIndex(It.IsAny<int>())).Returns(expectedCard); // Mock getting card from index
+            mockCardDeck.Setup(deck => deck.RemoveCardFromIndex(It.IsAny<int>())); // Mock removing card from index
 
             // Act
-            var result = _tableService.GenerateCard();
+            var result = tableService.GenerateCard();
 
             // Assert
             Assert.That(expectedCard, Is.EqualTo(result)); // Ensure the correct card is returned
-            _mockCardDeck.Verify(deck => deck.GetCardFromIndex(It.IsAny<int>()), Times.Once); // Verify GetCardFromIndex was called once
-            _mockCardDeck.Verify(deck => deck.RemoveCardFromIndex(It.IsAny<int>()), Times.Once); // Verify RemoveCardFromIndex was called once
+            mockCardDeck.Verify(deck => deck.GetCardFromIndex(It.IsAny<int>()), Times.Once); // Verify GetCardFromIndex was called once
+            mockCardDeck.Verify(deck => deck.RemoveCardFromIndex(It.IsAny<int>()), Times.Once); // Verify RemoveCardFromIndex was called once
         }
 
         [Test]
@@ -56,18 +56,19 @@ namespace TestingBeclean
             var possibleCards = new List<PlayingCard>
             {
                 new PlayingCard(PlayingCard.CARD_VALUES["A"], PlayingCard.HEART_SYMBOL),
-                new PlayingCard(PlayingCard.CARD_VALUES["K"],PlayingCard.HEART_SYMBOL),
+                new PlayingCard(PlayingCard.CARD_VALUES["K"], PlayingCard.HEART_SYMBOL),
+
                 // Add more cards as needed
             };
             var expectedMaxHandValue = new Tuple<int, int>(10, 1); // Example expected max hand value
-            _mockRankCalculator.Setup(rc => rc.GetValue(It.IsAny<List<PlayingCard>>())).Returns(expectedMaxHandValue); // Mock rank calculator to return expected max hand value
+            mockRankCalculator.Setup(rc => rc.GetValue(It.IsAny<List<PlayingCard>>())).Returns(expectedMaxHandValue); // Mock rank calculator to return expected max hand value
 
             // Act
-            var result = _tableService.DetermineMaxHand(possibleCards);
+            var result = tableService.DetermineMaxHand(possibleCards);
 
             // Assert
             Assert.That(expectedMaxHandValue, Is.EqualTo(result)); // Ensure the correct max hand value is returned
-            _mockRankCalculator.Verify(rc => rc.GetValue(It.IsAny<List<PlayingCard>>()), Times.AtLeastOnce); // Verify GetValue method was called at least once
+            mockRankCalculator.Verify(rc => rc.GetValue(It.IsAny<List<PlayingCard>>()), Times.AtLeastOnce); // Verify GetValue method was called at least once
         }
 
         [Test]
@@ -79,41 +80,44 @@ namespace TestingBeclean
             var player2 = new User(userID: 2, userName: "Player2", userChips: 1500);
             var player3 = new User(userID: 3, userName: "Player3", userChips: 2000);
 
-            activePlayers.Enqueue(new MenuWindow(player1, _mockMainService.Object));
-            activePlayers.Enqueue(new MenuWindow(player2, _mockMainService.Object));
-            activePlayers.Enqueue(new MenuWindow(player3, _mockMainService.Object));
+            activePlayers.Enqueue(new MenuWindow(player1, mockMainService.Object));
+            activePlayers.Enqueue(new MenuWindow(player2, mockMainService.Object));
+            activePlayers.Enqueue(new MenuWindow(player3, mockMainService.Object));
 
             var expectedWinners = new List<User> { player2 };
             // Set up player 1 hand
-            var player1Hand = new List<PlayingCard> {
-            new PlayingCard(value: PlayingCard.CARD_VALUES["A"], suit: PlayingCard.HEART_SYMBOL),
-            new PlayingCard(value: PlayingCard.CARD_VALUES["K"], suit: PlayingCard.HEART_SYMBOL)
+            var player1Hand = new List<PlayingCard>
+            {
+                new PlayingCard(value: PlayingCard.CARD_VALUES["A"], suit: PlayingCard.HEART_SYMBOL),
+                new PlayingCard(value: PlayingCard.CARD_VALUES["K"], suit: PlayingCard.HEART_SYMBOL)
             };
 
                     // Set up player 2 hand
-                    var player2Hand = new List<PlayingCard> {
-            new PlayingCard(value: PlayingCard.CARD_VALUES["Q"], suit: PlayingCard.DIAMOND_SYMBOL),
-            new PlayingCard(value: PlayingCard.CARD_VALUES["J"], suit: PlayingCard.DIAMOND_SYMBOL)
+            var player2Hand = new List<PlayingCard>
+            {
+                new PlayingCard(value: PlayingCard.CARD_VALUES["Q"], suit: PlayingCard.DIAMOND_SYMBOL),
+                new PlayingCard(value: PlayingCard.CARD_VALUES["J"], suit: PlayingCard.DIAMOND_SYMBOL)
             };
 
                     // Set up player 3 hand
-                    var player3Hand = new List<PlayingCard> {
-            new PlayingCard(value: PlayingCard.CARD_VALUES["10"], suit: PlayingCard.SPADE_SYMBOL),
-            new PlayingCard(value: PlayingCard.CARD_VALUES["9"], suit: PlayingCard.SPADE_SYMBOL)
+            var player3Hand = new List<PlayingCard>
+            {
+                new PlayingCard(value: PlayingCard.CARD_VALUES["10"], suit: PlayingCard.SPADE_SYMBOL),
+                new PlayingCard(value: PlayingCard.CARD_VALUES["9"], suit: PlayingCard.SPADE_SYMBOL)
             };
 
             // Set up hand values for players 1, 2, and 3
-            _mockRankCalculator.SetupSequence(rc => rc.GetValue(It.IsAny<List<PlayingCard>>()))
+            mockRankCalculator.SetupSequence(rc => rc.GetValue(It.IsAny<List<PlayingCard>>()))
                 .Returns(new Tuple<int, int>(10, 1)) // Hand value for player 1
-                .Returns(new Tuple<int, int>(9, 2))  // Hand value for player 2
+                .Returns(new Tuple<int, int>(9, 2)) // Hand value for player 2
                 .Returns(new Tuple<int, int>(8, 3)); // Hand value for player 3
 
             // Act
-            var result = _tableService.DetermineWinners(activePlayers);
+            var result = tableService.DetermineWinners(activePlayers);
 
             // Assert
             Assert.That(expectedWinners, Is.EqualTo(result)); // Ensure the correct winners are returned
-            _mockRankCalculator.Verify(rc => rc.GetValue(It.IsAny<List<PlayingCard>>()), Times.Exactly(3)); // Verify GetValue method was called exactly 3 times
+            mockRankCalculator.Verify(rc => rc.GetValue(It.IsAny<List<PlayingCard>>()), Times.Exactly(3)); // Verify GetValue method was called exactly 3 times
         }
 
         [Test]
@@ -126,12 +130,12 @@ namespace TestingBeclean
             mockMenuWindow.Setup(mw => mw.Player()).Returns(player);
 
             // Set up database service behavior
-            _mockDatabaseService.Setup(ds => ds.UpdateUserChips(It.IsAny<int>(), It.IsAny<int>())).Verifiable();
-            _mockDatabaseService.Setup(ds => ds.UpdateUserStack(It.IsAny<int>(), It.IsAny<int>())).Verifiable();
+            mockDatabaseService.Setup(ds => ds.UpdateUserChips(It.IsAny<int>(), It.IsAny<int>())).Verifiable();
+            mockDatabaseService.Setup(ds => ds.UpdateUserStack(It.IsAny<int>(), It.IsAny<int>())).Verifiable();
 
             // Act
-            var sqlConnection = _mockSqlConnection.Object;
-            int result = _tableService.JoinTable(mockMenuWindow.Object, ref sqlConnection);
+            var sqlConnection = mockSqlConnection.Object;
+            int result = tableService.JoinTable(mockMenuWindow.Object, ref sqlConnection);
 
             // Assert
             Assert.That(1, Is.EqualTo(result)); // Check that the return value indicates successful joining
@@ -139,8 +143,8 @@ namespace TestingBeclean
             Assert.That(1, Is.EqualTo(player.UserStatus)); // Check that the player's status is set to WAITING
 
             // Verify that database service methods were called
-            _mockDatabaseService.Verify(ds => ds.UpdateUserChips(player.UserID, player.UserChips), Times.Once);
-            _mockDatabaseService.Verify(ds => ds.UpdateUserStack(player.UserID, player.UserStack), Times.Once);
+            mockDatabaseService.Verify(ds => ds.UpdateUserChips(player.UserID, player.UserChips), Times.Once);
+            mockDatabaseService.Verify(ds => ds.UpdateUserStack(player.UserID, player.UserStack), Times.Once);
         }
 
         [Test]
@@ -148,16 +152,16 @@ namespace TestingBeclean
         {
             // Arrange
             // Set up TableService instance to be full
-            var sqlConnection = _mockSqlConnection.Object;
+            var sqlConnection = mockSqlConnection.Object;
             for (int i = 1; i <= 8; i++)
             {
-                _tableService.JoinTable(new Mock<MenuWindow>().Object, ref sqlConnection);
+                tableService.JoinTable(new Mock<MenuWindow>().Object, ref sqlConnection);
             }
 
             var mockMenuWindow = new Mock<MenuWindow>();
 
             // Act
-            int result = _tableService.JoinTable(mockMenuWindow.Object, ref sqlConnection);
+            int result = tableService.JoinTable(mockMenuWindow.Object, ref sqlConnection);
 
             // Assert
             Assert.That(0, Is.EqualTo(result)); // Check that the return value indicates failure to join
@@ -173,8 +177,8 @@ namespace TestingBeclean
             mockMenuWindow.Setup(mw => mw.Player()).Returns(player);
 
             // Act
-            var sqlConnection = _mockSqlConnection.Object;
-            int result = _tableService.JoinTable(mockMenuWindow.Object, ref sqlConnection);
+            var sqlConnection = mockSqlConnection.Object;
+            int result = tableService.JoinTable(mockMenuWindow.Object, ref sqlConnection);
 
             // Assert
             Assert.That(-1, Is.EqualTo(result)); // Check that the return value indicates insufficient chips
@@ -185,12 +189,12 @@ namespace TestingBeclean
         {
             // Arrange
             // Set up the users list with less than FULL users
-            var sqlConnection = _mockSqlConnection.Object;
-            _tableService.JoinTable(new Mock<MenuWindow>().Object, ref sqlConnection);
-            _tableService.JoinTable(new Mock<MenuWindow>().Object, ref sqlConnection);
+            var sqlConnection = mockSqlConnection.Object;
+            tableService.JoinTable(new Mock<MenuWindow>().Object, ref sqlConnection);
+            tableService.JoinTable(new Mock<MenuWindow>().Object, ref sqlConnection);
 
             // Act
-            bool result = _tableService.IsFull();
+            bool result = tableService.IsFull();
 
             // Assert
             ClassicAssert.IsFalse(result);
@@ -201,14 +205,14 @@ namespace TestingBeclean
         {
             // Arrange
             // Set up the users list with FULL users
-            var sqlConnection = _mockSqlConnection.Object;
+            var sqlConnection = mockSqlConnection.Object;
             for (int i = 1; i <= 8; i++)
             {
-                _tableService.JoinTable(new Mock<MenuWindow>().Object, ref sqlConnection);
+                tableService.JoinTable(new Mock<MenuWindow>().Object, ref sqlConnection);
             }
 
             // Act
-            bool result = _tableService.IsFull();
+            bool result = tableService.IsFull();
 
             // Assert
             ClassicAssert.IsTrue(result);
@@ -220,7 +224,7 @@ namespace TestingBeclean
             // Arrange
 
             // Act
-            bool result = _tableService.IsEmpty();
+            bool result = tableService.IsEmpty();
 
             // Assert
             ClassicAssert.IsTrue(result);
@@ -230,11 +234,11 @@ namespace TestingBeclean
         public void IsEmpty_UsersExist_ReturnsFalse()
         {
             // Arrange
-            var sqlConnection = _mockSqlConnection.Object;
-            _tableService.JoinTable(new Mock<MenuWindow>().Object, ref sqlConnection);
+            var sqlConnection = mockSqlConnection.Object;
+            tableService.JoinTable(new Mock<MenuWindow>().Object, ref sqlConnection);
 
             // Act
-            bool result = _tableService.IsEmpty();
+            bool result = tableService.IsEmpty();
 
             // Assert
             ClassicAssert.IsFalse(result);
@@ -246,7 +250,7 @@ namespace TestingBeclean
             // Arrange
 
             // Act
-            int result = _tableService.Occupied();
+            int result = tableService.Occupied();
 
             // Assert
             ClassicAssert.That(result, Is.EqualTo(0));
@@ -256,12 +260,12 @@ namespace TestingBeclean
         public void Occupied_UsersExist_ReturnsCorrectCount()
         {
             // Arrange
-            var sqlConnection = _mockSqlConnection.Object;
-            _tableService.JoinTable(new Mock<MenuWindow>().Object, ref sqlConnection);
-            _tableService.JoinTable(new Mock<MenuWindow>().Object, ref sqlConnection);
+            var sqlConnection = mockSqlConnection.Object;
+            tableService.JoinTable(new Mock<MenuWindow>().Object, ref sqlConnection);
+            tableService.JoinTable(new Mock<MenuWindow>().Object, ref sqlConnection);
 
             // Act
-            int result = _tableService.Occupied();
+            int result = tableService.Occupied();
 
             // Assert
             ClassicAssert.That(result, Is.EqualTo(2));
